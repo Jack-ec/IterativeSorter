06fff6352acf3dec59aa1cc6f18c50e623aa7f83
904154001 <904154001@L829WD150074.edu.cbe.ab.ca>
2021-10-20 09:27:54 -0600

    bubble

--- IterativeSorter/IterativeSorter/src/IterativeSorter.java
+++ IterativeSorter/IterativeSorter/src/IterativeSorter.java
@@ -1,8 +1,16 @@
 public class IterativeSorter {
 	
 	public void doBubbleSort(IntegerArray arrayToSort) {
-	}
-
+		 int n = arrayToSort.length();
+	        for (int i = 0; i < n-1; i++)
+	            for (int j = 0; j < n-i-1; j++)
+	                if (arrayToSort.read(j) > arrayToSort.read(j + 1))
+	                {
+	                    int temp = arrayToSort.read(j);
+	                    arrayToSort.write(j, arrayToSort.read(j + 1));
+	                    arrayToSort.write(j + 1, temp);
+	                }
+	    }
 	public void doSelectionSort(IntegerArray arrayToSort) {
 	}
 
